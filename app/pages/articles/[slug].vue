5f1bea3331173cc7cf6dcfcb4eb0684ccc9b4a2e
MCredbear <2363764471@qq.com>
2025-03-25 17:52:31 +0800

    加了一个 TOC

--- app/pages/articles/[slug].vue
+++ app/pages/articles/[slug].vue
@@ -3,15 +3,15 @@
     style="background-image: url('/usagi.jpg'); background-size: cover; background-position: center; height: 100vh; width: 100vw; overflow-y: auto;">
     <UHeader title="MCredbear 的神必地带" />
     <UPage>
-      <template #left>
+      <template #right>
         <UPageAside>
           <UCard class="bg-(--ui-bg)/75 backdrop-blur ml-15">
-            <UContentNavigation :navigation="sideNavigation" highlight highlight-color="primary" color="primary"
-              variant="pill" />
+            <!-- <UContentNavigation :navigation="sideNavigation" highlight highlight-color="primary" color="primary"
+              variant="pill" /> -->
+              <UContentToc :links="markdown.body.toc.links"/>
           </UCard>
         </UPageAside>
       </template>
-      <template #right />
 
       <UCard class="bg-(--ui-bg)/75 backdrop-blur mt-8">
         <ContentRenderer class="prose dark:prose-invert max-w-screen" :value="markdown" />
@@ -29,64 +29,29 @@
 <script setup>
 const slug = useRoute().params.slug
 
-// const markdown = ref(null);
-
-// onMounted(async () => {
-//   markdown.value = await queryCollection('blog').path(`/articles/${slug}`).first();
-// });
-
+const sideNavigation = ref([]);
 
 const { data: markdown } = await useAsyncData(`articles-${slug}`, async () => {
-  // return queryCollection('blog').where('stem', '==', `articles/${slug}`).first();
-  return queryCollection('article').path(`articles/${slug}`).first();
+  console.log(slug);
+  const result = await queryCollection('article').path(`articles/${slug}`).first();
+
+  // function convertNode(node) {
+  //   const result = {
+  //     title: node.text,
+  //     path: `#${node.id}`
+  //   };
+
+  //   if (node.children && node.children.length > 0) {
+  //     result.children = node.children.map(convertNode);
+  //   }
+
+  //   return result;
+  // };
+
+  // sideNavigation.value = result.body.toc.links.map(convertNode);
+
+  return result;
 })
 
-// import MarkdownIt from 'markdown-it';
-// import frontMatter from 'markdown-it-front-matter';
-// import hljs from 'highlight.js';
-// import 'highlight.js/styles/atom-one-dark.css';
-
-const sideNavigation = inject('sideNavigation');
-
-// const slug = useRoute().params.slug;
-
-// const markdownText = ref('');
-
-// const md = new MarkdownIt({
-//   highlight: function (str, lang) {
-//     if (lang && hljs.getLanguage(lang)) {
-//       try {
-//         return hljs.highlight(lang, str).value;
-//       } catch (__) { }
-//     }
-
-//     return '';
-//   }
-// });
-
-// md.use(frontMatter, fm => { });
-
-// const fetchMarkdown = async (url) => {
-//   try {
-//     const response = await fetch(url);
-//     if (response.ok) {
-//       const text = await response.text();
-//       markdownText.value = text;
-//     } else {
-//       console.error('Failed to fetch Markdown file:', response.status);
-//     }
-//   } catch (error) {
-//     console.error('Error fetching Markdown:', error);
-//   }
-// };
-
-// const renderMarkdown = (text) => {
-//   return md.render(text);
-// };
-
-// onMounted(() => {
-//   const markdownUrl = `/articles/${slug}.md`;
-//   fetchMarkdown(markdownUrl);
-// });
 
 </script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -17,7 +17,7 @@
       </template>
       <UPageList class="mt-8">
         <UBlogPost class="mb-5" v-for="article in articles" :key="article.id" :date="article.date"
-            :title="article.title" :description="article.description" :to="article.path" :badge="article.meta.tag"
+          :title="article.title" :description="article.description" :to="article.path" :badge="article.meta.tag"
           target="_blank" />
       </UPageList>
     </UPage>
@@ -25,20 +25,47 @@
 </template>
 
 <script setup>
-const sideNavigation = inject('sideNavigation');
+
+const sideNavigation = ref([
+  {
+    title: '神必网站',
+    icon: 'i-lucide-globe',
+    children: [
+      {
+        title: '+ 1 s 舞萌科技',
+        path: 'https://maitech.plus1second.icu',
+      },
+    ],
+  },
+  {
+    title: '友链',
+    icon: 'i-lucide-users',
+    children: [
+      {
+        title: 'NekoMoyi',
+        path: 'https://github.com/NekoMoYi'
+      },
+    ],
+  },
+  {
+    title: '关于',
+    icon: 'i-lucide-user',
+    path: '/about',
+  },
+]);
 
 const articles = ref([]);
 
 onMounted(async () => {
-  articles.value = await queryCollection('article').order("date", "DESC").select('title','description','date','meta','path').all();
-  window.addEventListener('hashchange', async () => {
-    articles.value = await queryCollection('article').order("date", "DESC").select('title','description','date','meta','path').all();
-    if (window.location.hash == '#programing') {
-      articles.value = articles.value.filter(article => article.meta.tag == 'Programing');
-    } else if (window.location.hash == '#others') {
-      articles.value = articles.value.filter(article => article.meta.tag != 'Programing');
-    }
-  });
+  articles.value = await queryCollection('article').order("date", "DESC").select('title', 'description', 'date', 'meta', 'path').all();
+  // window.addEventListener('hashchange', async () => {
+  //   articles.value = await queryCollection('article').order("date", "DESC").select('title', 'description', 'date', 'meta', 'path').all();
+  //   if (window.location.hash == '#programing') {
+  //     articles.value = articles.value.filter(article => article.meta.tag == 'Programing');
+  //   } else if (window.location.hash == '#others') {
+  //     articles.value = articles.value.filter(article => article.meta.tag != 'Programing');
+  //   }
+  // });
 });
 
 
